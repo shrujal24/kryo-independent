b58e58b65aa1b37fcaf332b6e7c73d98a93c50ee
shrujal24 <shrujalbhandari@gmail.com>
2025-11-02 02:01:57 -0500

    replace generic exceptions with specific library exceptions

---
+++ benchmarks/src/main/SerializationException.java
@@ -0,0 +1,7 @@
+package com.esotericsoftware.kryo.benchmarks;
+
+public class SerializationException extends RuntimeException {
+    public SerializationException(String message, Throwable cause) {
+        super(message, cause);
+    }
+}
